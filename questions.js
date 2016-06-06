7069fd1c9094a5936e7d59a14384692b21b40854
Johanne BADIN <johanne.b@simplon.co>
2016-06-06 09:45:08 +0200

    Ajout des 11 exercices

---
+++ questions.js
@@ -0,0 +1,55 @@
+var CreationTableauLangages = function () {
+
+  return 'A compléter';
+}
+
+var CreationTableauNombres = function () {
+
+    return 'A compléter';
+}
+
+var RemplacementElement = function (langages) {
+
+  return 'A compléter';
+}
+
+var AjoutElementLangages = function (langages) {
+
+  return 'A compléter';
+}
+
+var AjoutElementNombres = function (nombres) {
+
+  return 'A compléter';
+}
+
+var SuppressionPremierElement = function (langages) {
+
+  return 'A compléter';
+}
+
+var SuppressionDernierElement = function (langages) {
+
+  return 'A compléter';
+}
+
+var ConversionChaineTableau = function (reseaux_sociaux_chaine) {
+
+  return 'A compléter';
+}
+
+var ConversionTableauChaine = function (langages) {
+
+  return 'A compléter';
+}
+
+var TriTableau = function (reseaux_sociaux) {
+
+  return 'A compléter';
+}
+
+var InversionTableau = function (reseaux_sociaux){
+
+
+  return 'A compléter';
+}
